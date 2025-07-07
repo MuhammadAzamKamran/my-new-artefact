c4631b25976c050087769788ce79adcd650c26e2
MuhammadAzamKamran <164350812+MuhammadAzamKamran@users.noreply.github.com>
2025-07-07 17:43:55 +0100

    Made some changes

--- basket.php
+++ basket.php
@@ -70,6 +70,7 @@ if (isset($_SESSION['basket'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport">
     <title>Computer Equipment & Games</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
     rel="stylesheet">
